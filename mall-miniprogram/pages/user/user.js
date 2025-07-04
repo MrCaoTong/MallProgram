5ebf6ceb84bf60d26d5df8d77ecf4cbafb6fa433
MrCaoTong <13246684493@163.com>
2025-07-04 18:46:42 +0800

    增加微信登录功能(待真机测试)

--- mall-miniprogram/pages/user/user.js
+++ mall-miniprogram/pages/user/user.js
@@ -1,6 +1,90 @@
 Page({
-  data: {},
+  data: {
+    isLogin: false,
+    userInfo: {},
+    openidReady: false
+  },
   onLoad() {
-    // 个人中心页初始化
+    wx.login({
+      success: res => {
+        if (res.code) {
+          wx.request({
+            url: 'http://localhost:4000/api/user/login',
+            method: 'POST',
+            data: { code: res.code },
+            success: resp => {
+              if (resp.data.code === 0) {
+                wx.setStorageSync('openid', resp.data.data.openid);
+                this.setData({ openidReady: true });
+                console.log('onLoad获取到openid:', resp.data.data.openid);
+              } else {
+                console.log('onLoad获取openid失败', resp.data);
+              }
+            }
+          });
+        }
+      }
+    });
+    // 检查本地是否已登录
+    const userInfo = wx.getStorageSync('userInfo');
+    if (userInfo) {
+      this.setData({ isLogin: true, userInfo });
+    }
+  },
+  onShow() {
+    // 每次显示时同步登录状态
+    const userInfo = wx.getStorageSync('userInfo');
+    this.setData({ isLogin: !!userInfo, userInfo: userInfo || {} });
+    console.log('onShow userInfo', this.data.userInfo);
+  },
+  onGetUserInfo() {
+    console.log('点击登录按钮');
+    const openid = wx.getStorageSync('openid');
+    console.log('onGetUserInfo读取到openid:', openid);
+    if (!openid) {
+      wx.showToast({ title: 'openid未获取到，请稍后再试', icon: 'none' });
+      return;
+    }
+    wx.getUserProfile({
+      desc: '用于完善会员资料',
+      success: res => {
+        console.log('getUserProfile返回', res);
+        wx.request({
+          url: 'http://localhost:4000/api/user/info',
+          method: 'POST',
+          data: {
+            openid,
+            avatarUrl: res.userInfo.avatarUrl,
+            nickName: res.userInfo.nickName
+          },
+          success: resp => {
+            console.log('后端返回', resp);
+            if (resp.data.code === 0) {
+              wx.setStorageSync('userInfo', res.userInfo);
+              this.setData({ isLogin: true, userInfo: res.userInfo });
+              console.log('setData后 userInfo', this.data.userInfo);
+              wx.showToast({ title: '登录成功', icon: 'success' });
+            }
+          }
+        });
+      },
+      fail: err => {
+        console.log('getUserProfile失败', err);
+      }
+    });
+  },
+  logout() {
+    wx.removeStorageSync('userInfo');
+    this.setData({ isLogin: false, userInfo: {} });
+    wx.showToast({ title: '已退出登录', icon: 'none' });
+  },
+  goOrder() {
+    wx.navigateTo({ url: '/pages/order/order' });
+  },
+  goFavorite() {
+    wx.showToast({ title: '我的收藏功能待实现', icon: 'none' });
+  },
+  goAddress() {
+    wx.navigateTo({ url: '/pages/address/address' });
   }
 }); 
--- mall-server/src/routes/user.js
+++ mall-server/src/routes/user.js
@@ -1,9 +1,49 @@
+require('dotenv').config();
 const express = require('express');
+const axios = require('axios');
+const router = express.Router();
+const pool = require('../config/db');
 const userController = require('../controllers/user');
 const auth = require('../middlewares/auth');
-const router = express.Router();
 
-router.post('/login', userController.login);
+// 微信小程序配置
+const appid = process.env.WECHAT_APPID;
+const secret = process.env.WECHAT_SECRET;
+
+// 1. code换openid
+router.post('/login', async (req, res) => {
+  const { code } = req.body;
+  if (!code) return res.json({ code: 1, msg: 'code必传' });
+  try {
+    const wxResp = await axios.get(
+      `https://api.weixin.qq.com/sns/jscode2session?appid=${appid}&secret=${secret}&js_code=${code}&grant_type=authorization_code`
+    );
+    console.log('微信返回:', wxResp.data); // 打印微信接口返回内容
+    const { openid } = wxResp.data;
+    if (!openid) return res.json({ code: 2, msg: '获取openid失败', data: wxResp.data });
+    res.json({ code: 0, data: { openid } });
+  } catch (err) {
+    res.json({ code: 500, msg: '微信登录失败', error: err });
+  }
+});
+
+// 2. 保存用户信息
+router.post('/info', async (req, res) => {
+  const { openid, avatarUrl, nickName } = req.body;
+  if (!openid) return res.json({ code: 1, msg: 'openid必传' });
+  try {
+    const [rows] = await pool.query('SELECT * FROM user WHERE openid=?', [openid]);
+    if (rows.length > 0) {
+      await pool.query('UPDATE user SET avatar=?, nickname=? WHERE openid=?', [avatarUrl, nickName, openid]);
+    } else {
+      await pool.query('INSERT INTO user (openid, avatar, nickname) VALUES (?, ?, ?)', [openid, avatarUrl, nickName]);
+    }
+    res.json({ code: 0, msg: 'ok', userInfo: { openid, avatarUrl, nickName } });
+  } catch (err) {
+    res.json({ code: 500, msg: '保存用户信息失败', error: err });
+  }
+});
+
 router.get('/info', auth.jwtAuth, userController.info);
 router.post('/logout', auth.jwtAuth, userController.logout);
 
